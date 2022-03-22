b96a18b217fe57338a1158a60a6f5d99f62d48c1
bakr <you@example.com>
2022-03-22 23:48:57 +0200

    yes

--- pages/api/product.js
+++ pages/api/product.js
@@ -5,13 +5,12 @@ export default async function handler(req, res) {
     const product = JSON.parse(req.body);
     let productDoc = "";
     if (product.id) {
-      console.log("aywa hnaaaaaa=", product.id);
       productDoc = await db.collection("products").doc(product.id);
     } else {
       productDoc = await db.collection("products").doc();
     }
 
-    productDoc.set({
+    let ttt = await productDoc.set({
       ...product,
       id: productDoc.id,
       salePrice: Math.round(
@@ -19,6 +18,7 @@ export default async function handler(req, res) {
       ),
       lastEditDate: +new Date(),
     });
+    console.log(ttt, "aywa hnaaaaaa=", product.id);
     res.status(200).json({ id: productDoc.id });
   } catch (e) {
     res.status(400).json({ e: e });
